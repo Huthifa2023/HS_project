a63f26b232b5199a63c456dc1da38e66b2a8e3e5
Huthifa Alqasim <huthifa2013@gmail.com>
2023-12-11 15:56:06 +0200

    message style shahrazad

--- progress_project/progress_app/static/js/script.js
+++ progress_project/progress_app/static/js/script.js
@@ -20,7 +20,7 @@ hamburger.addEventListener("click", toggleMenu);
 
 
 // slider
-$('.carousel').carousel()
+// $('.carousel').carousel()
 
 
 
@@ -53,3 +53,10 @@ signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
 
+//$(".close").click(function(){
+ //   $(this).parent().fadeOut();
+ //});
+
+$( "button.close" ).on( "click", function() {
+    $( "div.alert" ).fadeOut("slow")
+    });
